021a647dab702bfccd2664809b3ab789dca7e268
mohamed elbahnasy <117590548+mohamedhussein222@users.noreply.github.com>
2023-03-28 17:05:20 +0200

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,31 +1,30 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-
+import Home from './Home/Home';
 class Navbar extends Component {
 
     render() {
-        return <div>
+        return <>
 
-<nav class="navbar navbar-expand-lg  fixed-top z-2">
-        <div class="container-lg navbar-container w-75">
-          <a class="navbar-brand" href="#">START REACT</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+<nav className="navbar navbar-expand-lg fixed-top z-2">
+        <div className="container-lg navbar-container w-75">
+          <Link className="navbar-brand" to="home">START REACT</Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active mx-2" aria-current="page" href="#home">PORTFOLIO</a>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link active mx-2" aria-current="page" to="portfolio">Portfolio</Link>
               </li>
              
-              <li class="nav-item mx-2">
-                <a class="nav-link" href="#about">About</a>
+              <li className="nav-item mx-2">
+                <Link className="nav-link" to="about">About</Link>
               </li>
-              <li class="nav-item mx-2">
-              <a class="nav-link" href="#services">Contact</a>
+              <li className="nav-item mx-2">
+                <Link className="nav-link" to="contact">Contact</Link>
               </li>
-              
-              
+            
             
              
             </ul>
@@ -33,8 +32,11 @@ class Navbar extends Component {
           </div>
         </div>
       </nav>
-        </div>
-    
+
+
+
+<Home/>
+</>
       
     }
 }
